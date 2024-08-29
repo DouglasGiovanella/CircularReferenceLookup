24318e272b07bfd6ed4dd1f24723d890afdba8bd
Douglas Giovanella <douglas.giovanella@asaas.com.br>
2024-08-29 19:07:32 -0300

    asdasd

---
+++ asaas/domain/grails-app/services/Test1.groovy
@@ -0,0 +1,10 @@
+package com.asaas.service.pix
+
+import grails.transaction.Transactional
+
+@Transactional
+class Test1 {
+
+    def test2
+
+}
---
+++ asaas/domain/grails-app/services/Test2.groovy
@@ -0,0 +1,10 @@
+package com.asaas.service.pix
+
+import grails.transaction.Transactional
+
+@Transactional
+class Test2 {
+
+    Test3 test3
+
+}
